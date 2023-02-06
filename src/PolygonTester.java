57285681be689f1f13c75e95b88c776e26810de1
phippsa-wlhs <phippsa@wlhs.wlwv.k12.or.us>
2023-02-06 08:38:06 -0800

    comments

--- src/PolygonTester.java
+++ src/PolygonTester.java
@@ -1,6 +1,8 @@
 public class PolygonTester {
     public static void main(String[] args) {
-        //test your polygon here
+        /**
+         * test your polygon here
+         */
         Polygon polygon1 = new Polygon(5,6.865,"pentagon");
         System.out.println(polygon1);
     }
